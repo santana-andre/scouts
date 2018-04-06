13b46ccaa2e272dea209338d7767c27a455329f9
Allan Aievoli <allan@webjump.com.br>
2018-04-06 20:27:10 -0300

    home initial

---
+++ application/views/public/_parts/public_master_header_view.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title><?php echo $page_title;?></title>
+<link href="<?php echo site_url('assets/admin/css/bootstrap.min.css');?>" rel="stylesheet">
+<?php echo $before_head;?>
+</head>
+<body>
--- application/views/public/homepage_view.php
+++ application/views/public/homepage_view.php
@@ -1,6 +1,12 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');?>
+<?php 
+	defined('BASEPATH') OR exit('No direct script access allowed'); 
+	$this->load->view('public/_parts/public_master_header_view'); 
+?>
+
 <div class="container" style="margin-top: 60px;">
 <?php
 echo $this->lang->line('homepage_welcome');
 ?>
 </div>
+
+<?php  $this->load->view('public/_parts/public_master_footer_view');  ?>
